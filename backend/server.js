f5b9b3f33484afc71368cdce7647fb929bdd95cb
Abrar Shaikh <abrarshaikh9825@gmail.com>
2024-10-02 21:51:10 +0530

    Add Product 2-10-2024

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js'; // Import database connection
+import cors from 'cors';
 
 import productRoutes from "./routes/product.route.js";
 dotenv.config(); // Load environment variables
 
 const app = express();
 const PORT = process.env.PORT || 5000; // Use port from env or default to 5000
+app.use(cors());
 
 app.use(express.json()); // Middleware for parsing JSON
 app.use("/api/products", productRoutes)
---
+++ frontend/src/store/product.js
@@ -0,0 +1,35 @@
+import { create } from "zustand";
+
+export const useProductStore = create((set) => ({
+	products: [],
+	setProducts: (products) => set({ products }),
+	createProduct: async (newProduct) => {
+    if (!newProduct.name || !newProduct.image || !newProduct.price) {
+        return { success: false, message: "Please fill in all fields." };
+    }
+    
+    try {
+        const res = await fetch("/api/products", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newProduct),
+        });
+
+        if (!res.ok) {
+            const errorData = await res.json();
+            throw new Error(errorData.message || "Failed to create product.");
+        }
+
+        const data = await res.json();
+        set((state) => ({ products: [...state.products, data.data] }));
+        return { success: true, message: "Product created successfully" };
+    } catch (error) {
+        console.error("Error creating product:", error);
+        return { success: false, message: error.message };
+    }
+}
+
+
+}));
